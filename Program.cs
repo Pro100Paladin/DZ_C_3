8e9e45ff79e1a27a9ff8cc1cce34f404600e2a7d
Pro100Paladin <pro100paladin@icloud.com>
2023-07-27 21:15:52 +0300

    решение задачи 23

--- Program.cs
+++ Program.cs
@@ -4,3 +4,14 @@
 5 -> 1, 8, 27, 64, 125*/
 
 
+Console.WriteLine("Введите число ");
+int num = int.Parse(Console.ReadLine()!);
+
+int index = 1;
+
+while (index <= num)
+{
+    Console.WriteLine(Math.Pow(index, 3));
+    index++;
+}
+ 
